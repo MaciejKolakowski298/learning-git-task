c9c8a0f2e786ba68cfaebd23b9be0a5ae6a0b22d
Maciej Kolakowski <maciej.kolakowski@gmail.com>
2022-11-21 22:33:39 +0100

    Add petla sklep>zakupy

--- lista-zakupow.py
+++ lista-zakupow.py
@@ -4,3 +4,8 @@ lista_zakupow={
     "Warzywniak":['Marchew','Seler','Rukola']
 }
 print("Lista zakupów")
+for key in lista_zakupow:
+    if key=="Piekarnia":
+        print(f"Idę do Piekarnia, kupuję tu następujące rzeczy: {(lista_zakupow['Piekarnia'])}")
+    elif key=="Warzywniak":
+        print(f"Idę do Warzywniak, kupuję tu następujące rzeczy: {lista_zakupow['Warzywniak']}")
